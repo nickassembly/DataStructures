c480a42052f8cf92b69886b704b17a64da366563
nickguerra <nickg@maliciousgames.com>
2023-05-27 05:17:00 -0500

    added linked list

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -2,4 +2,13 @@
 Console.WriteLine("Hello, Keller!");
 
 
+// adding a linked list of strings
+var linkedList = new LinkedList<string>();
+linkedList.AddLast("Hello");
+linkedList.AddLast("Keller");
+linkedList.AddLast("!");
 
+foreach (var item in linkedList)
+{
+    Console.WriteLine(item);
+}
