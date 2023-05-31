538cc56aee32f65f583a5fc487d1695762a672fd
nickguerra <nickg@maliciousgames.com>
2023-05-30 20:41:56 -0500

    Added node class

---
+++ DataStructures/ArrayMethods/LinearSearch.cs
@@ -0,0 +1,20 @@
+﻿namespace DataStructures.ArrayMethods
+{
+    public class ArrayMethods
+    {
+        public static bool LinearSearch(int[] array, int key)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == key)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,2 +1,20 @@
 ﻿
-// Linear searching
+using DataStructures.ArrayMethods;
+using DataStructures.LinkedList;
+
+int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//Console.WriteLine(ArrayMethods.LinearSearch(array, 5)); // true
+
+// LinkedList
+Node nodeA = new Node();
+nodeA.Data = 23421;
+
+Node nodeB = new Node();
+nodeB.Data = 4522;
+
+Node nodeC = new Node();
+nodeC.Data = 1233;
+
+Node nodeD = new Node();
+nodeD.Data = 9872;
