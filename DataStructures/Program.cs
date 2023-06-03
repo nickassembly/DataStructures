707baeb631fdf1c667da2b89cf88c7244cc4023e
nickguerra <nickg@maliciousgames.com>
2023-06-03 10:35:31 -0500

    Added bubble sort function

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -62,3 +62,28 @@ using DataStructures.Stack;
 //Console.WriteLine(bst.Find(151));
 
 //int value = 0;
+
+int[] intArray = new int[] { 6, 5, 1, 7, 2, 4 };
+
+Console.WriteLine(BubbleSort(intArray));
+int[] value = BubbleSort(intArray);
+
+int[] BubbleSort(int[] array)
+{
+    int temp = 0;
+
+    for (int pointer = 0; pointer < array.Length; pointer++)
+    {
+        for (int sort = 0; sort < array.Length - 1; sort++)
+        {
+            if (array[sort] > array[sort + 1])
+            {
+                temp = array[sort + 1];
+                array[sort + 1] = array[sort];
+                array[sort] = temp;
+            }
+        }
+    }
+
+    return array;
+}
